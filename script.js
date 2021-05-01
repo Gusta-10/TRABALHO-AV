9f242e87eeaea366bb85d0fa918543b76541f254
Gusta-10 <83405462+Gusta-10@users.noreply.github.com>
2021-05-01 20:28:14 -0300

    Finalização do trabalho av1
    Acrescentei algumas coisas e retirei outras

--- script.js
+++ script.js
@@ -18,29 +18,40 @@ function myFunction() {
 }
 
 function comprar(){
-  let pcalabresa, pportuguesa,pquatroqueijos,p1,p2,p3,pd1,pd2,pd3;
+  let pcalabresa, pportuguesa,pquatroqueijos,p1,p2,p3,pd1,pd2,pd3,total;
   pcalabresa = document.getElementById("pcalabresa").value;
   pportuguesa = document.getElementById("pportuguesa").value;
   pquatroqueijos = document.getElementById("pquatroqueijos").value;
+  p1 =  parseInt(pcalabresa) * 35
+  p2 =  parseInt(pportuguesa) * 40
+  p3 =  parseInt(pquatroqueijos) * 45
 
  if (pcalabresa >= 1) {
-   p1 =  parseInt(pcalabresa) * 35
-   console.log(p1)
+   
+   pd1 = document.getElementById("pedido1");
+   pd1.innerHTML="R$" + p1 +",00 O VALOR DO SEU PEDIDO DE PIZZA DE CALABRESA"
+  ELSE (pcalabresa = 0)
+   pd1 = document.getElementById("pedido1");
+   pd1.innerHTML="1° PEDIDO APARECERÁ AQUI!!"
   if (pportuguesa >= 1) {
-   p2 =  parseInt(pportuguesa) * 40
-   console.log(p2)
+  
+   pd2 = document.getElementById("pedido2");
+   pd2.innerHTML="R$" + p2 +",00 O VALOR DO SEU PEDIDO DE PIZZA DE PORTUGUESA"
    }
     if (pquatroqueijos >= 1) {
-     p3 =  parseInt(pquatroqueijos) * 45
-     console.log(p3)
-    }
-    if (pcalabresa >=1){
-  pd1 = document.getElementById("pedido1");
-  pd1.innerHTML='1 PIZZA DE CALABRESA  VALOR R$35'
   
-  }
+     pd3 = document.getElementById("pedido3");
+     pd3.innerHTML="R$" + p3 +",00 O VALOR DO SEU PEDIDO DE PIZZA QUATRO QUEIJOS"
+    }
+    if (pcalabresa >= 1)
+    total=parseInt(p1) + parseInt(total)
+    total=document.getElementById("Totalapagar");
+    total.innerHTML="R$"+total+",00 A PAGAR"
+    }
+
+
  }
 
-}
+
 
  
